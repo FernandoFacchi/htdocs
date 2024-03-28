48d179ea406c96ac8e4372020bf93b4805a3da01
FernandoFacchi <160238501+FernandoFacchi@users.noreply.github.com>
2024-03-28 17:10:42 -0300

    Create index.php

---
+++ crud.03/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRUD</title>
+</head>
+  <body>
+    <header>
+      <h1>CADASTROS DE USUÁRIOS</h1>
+    </header>
+    <main>
+    <form action="dados.php" method="post">
+      <div class="form-group">
+        <label for="">Nome:</label>
+        <input type="text" name="nome">
+      </div>
+      <div class="form-group">
+        <label for="">Salário:</label>
+        <input type="text" name= "salario">
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Salvar">
+      </div>
+    </form>
+    </main>
+    <footer>
+
+    </footer>
+  </body>
+</html>
